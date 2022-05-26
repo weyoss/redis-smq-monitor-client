c98a977aca54517236cfd772ca8b2f72fd033bdf
Weyoss <weyoss@protonmail.com>
2022-05-26 21:36:52 +0100

    Fix start-monitor-server.ts helper

--- src/client/tools/start-monitor-server.ts
+++ src/client/tools/start-monitor-server.ts
@@ -1,10 +1,11 @@
 // Using redis-smq from a local repository
 // Fix this according to your preferences
-import { MonitorServer, setConfiguration, setLogger } from '../../../../redis-smq';
-import { IConfig, RedisClientName } from '../../../../redis-smq/dist/types';
+import { RedisClientName } from '../../../../redis-smq-common/dist/types';
+import { MonitorServer } from '../../../../redis-smq-monitor';
+import { TConfig } from '../../../../redis-smq-monitor/dist/types';
+import { logger } from '../../../../redis-smq-common';
 
-export const config: IConfig = {
-    namespace: 'ns1',
+export const config: TConfig = {
     redis: {
         client: RedisClientName.IOREDIS,
         options: {
@@ -15,15 +16,13 @@ export const config: IConfig = {
     logger: {
         enabled: true
     },
-    monitor: {
-        enabled: true,
+    server: {
         port: 3000,
         host: '127.0.0.1'
     }
 };
 
-setConfiguration(config);
-setLogger(console);
+logger.setLogger(console);
 
-const server = new MonitorServer();
+const server = MonitorServer.createInstance(config);
 server.listen();
