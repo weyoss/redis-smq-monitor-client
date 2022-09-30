1b70007f819693c4f9d516f5f19b3452a4ed679c
Weyoss <weyoss@protonmail.com>
2022-09-30 14:07:35 +0100

    feat(hooks): add custom useParams() hook

---
+++ src/client/hooks/useParams.ts
@@ -0,0 +1,7 @@
+import { matchPath, useLocation } from 'react-router';
+
+export function useParams(path: string) {
+    const { pathname } = useLocation()
+    const match = matchPath(pathname, { path })
+    return match?.params || {}
+}
