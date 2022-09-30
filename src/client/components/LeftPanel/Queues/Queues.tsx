6a631d36e9907d7214319d0d13b8eec9ee990cd6
Weyoss <weyoss@protonmail.com>
2022-09-30 14:26:04 +0100

    feat(LeftPanel/Queues): use custom useParams() hook

--- src/client/components/LeftPanel/Queues/Queues.tsx
+++ src/client/components/LeftPanel/Queues/Queues.tsx
@@ -1,16 +1,17 @@
 import React, { useCallback } from 'react';
-import { RouteComponentProps, useHistory, withRouter } from 'react-router';
+import { useHistory } from 'react-router';
 import QueuesPage from './QueuesPage';
 import { IStoreState } from '../../../store/state';
 import useSelector from '../../../hooks/useSelector';
-import { matchRoute } from '../../../routes/common';
 import { IWebsocketMainStreamState } from '../../../store/websocket-main-stream/state';
 import { addNotificationAction } from '../../../store/notifications/action';
 import { ENotificationType } from '../../../store/notifications/state';
 import { useDispatch } from 'react-redux';
 import { deleteNamespace } from '../../../transport/http/api';
+import { useParams } from '../../../hooks/useParams';
+import { queue } from '../../../routes/routes';
 
-const Queues: React.FC<RouteComponentProps> = (props) => {
+const Queues = () => {
     const { payload } = useSelector<IStoreState, IWebsocketMainStreamState>(
         (state) => state.websocketMainStream
     );
@@ -23,8 +24,7 @@ const Queues: React.FC<RouteComponentProps> = (props) => {
     }, [payload]);
     // This component is not a child of the Router, so we can not access current route parameters.
     // This is a workaround to get the parameters.
-    const match = matchRoute(props.location.pathname);
-    const params = match?.params ?? {};
+    const params = useParams(queue.path);
     return (
         <QueuesPage
             deleteNamespaceRequestCallback={deleteNamespaceRequestCallback}
@@ -35,4 +35,4 @@ const Queues: React.FC<RouteComponentProps> = (props) => {
     );
 };
 
-export default withRouter(Queues);
+export default Queues;
