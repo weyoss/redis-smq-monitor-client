708ba65415102fc23d99f7c94be175aa20ebf3e8
Ivan Zhuravlev <intech@users.noreply.github.com>
2023-12-16 23:45:29 +0400

    fix: path for purge acknowledged messages

--- src/client/transport/http/api/purge-messages.ts
+++ src/client/transport/http/api/purge-messages.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../../endpoints';
 
 export const purgeAcknowledgedMessages = async (ns: string, queueName: string) => {
-    return axios.delete(`${API_URL}/api/ns/${ns}/queues/${queueName}/pending-messages`);
+    return axios.delete(`${API_URL}/api/ns/${ns}/queues/${queueName}/acknowledged-messages`);
 };
 
 export const purgeDeadLetteredMessages = async (ns: string, queueName: string) => {
